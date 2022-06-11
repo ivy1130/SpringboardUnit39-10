08ce19102b4a63b1c9c118df551ed9566f052e04
Ivy <ivy@onprem.com>
2022-06-11 16:43:06 +0200

    Completed Step Four: useAxios in PokeDex.

--- src/PlayingCardList.js
+++ src/PlayingCardList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import useAxios from "./hooks/useAxios";
 import PlayingCard from "./PlayingCard";
 import "./PlayingCardList.css";
@@ -6,12 +6,12 @@ import "./PlayingCardList.css";
 /* Renders a list of playing cards.
  * Can also add a new card at random. */
 function CardTable() {
-  const [cards, addCard] = useAxios();
+  const [cards, addCard] = useAxios("https://deckofcardsapi.com/api/deck/new/draw/");
   return (
     <div className="PlayingCardList">
       <h3>Pick a card, any card!</h3>
       <div>
-        <button onClick={addCard}>Add a playing card!</button>
+        <button onClick={() => addCard()}>Add a playing card!</button>
       </div>
       <div className="PlayingCardList-card-area">
         {cards.map(cardData => (
--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -2,11 +2,11 @@ import { useState } from "react";
 import {v4 as uuid} from "uuid";
 import axios from "axios";
 
-const useAxios = (url, options = {}) => {
+const useAxios = (url) => {
   const [cards, setCards] = useState([])
-  const addCard = async () => {
+  const addCard = async (addlUrl= "") => {
     const response = await axios.get(
-      "https://deckofcardsapi.com/api/deck/new/draw/"
+      `${url}${addlUrl}`
     );
     setCards(cards => [...cards, { ...response.data, id: uuid() }]);
   };
